1501e122c53aa755b699a35b4535cae0021c0269
Lois Reid <lois-reid@outlook.com>
2020-04-29 09:39:53 -0400

    dev-001 - Initial Project Setup
    Implementation:
    - initial setup of projects
    - main scheduler project
    - test project
    - simple worker program
    - resources (config, release build location of worker program)

---
+++ AiCustomTaskScheduler/AiCustomTaskScheduler.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AiCustomTaskScheduler.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AiCustomTaskScheduler.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("be0d5910-c086-42f6-bf3c-3aee6c1f2f9f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AiCustomTaskScheduler/AiCustomTaskScheduler.Worker/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace AiCustomTaskScheduler.Worker
+{
+    /// <summary>
+    /// A simple program that simulates work by sleeping for 30 seconds.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Working - starting to sleep for 30s...");
+            Thread.Sleep(30000);
+            Console.WriteLine("Finished sleeping!");
+        }
+    }
+}
